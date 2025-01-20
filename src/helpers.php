3a01bbbe2f7ffee0bb8439b2f9878b4e285ece19
Olivier Gorzalka <olivier@amphibee.fr>
2025-01-20 13:52:04 +0100

    ♻️ (composer.json, helpers.php): remove unused autoloading and helper functions to clean up codebase and improve maintainability

--- src/helpers.php
+++ src/helpers.php
@@ -3,26 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Lang;
-use Pollora\Support\RecursiveMenuIterator;
-
-if (! function_exists('wp_mail')) {
-    function wp_mail($to, $subject, $message, $headers = '', $attachments = []): bool
-    {
-        $result = app('wp.mail')->send($to, $subject, $message, $headers, $attachments);
-
-        return $result !== null;
-    }
-}
-
-if (! function_exists('mysqli_report')) {
-    /**
-     * Report MySQL errors.
-     */
-    function mysqli_report(): void
-    {
-        // silence is golden
-    }
-}
 
 if (! function_exists('__')) {
     /**
@@ -54,29 +34,5 @@ if (! function_exists('__')) {
         $key = str_replace('wordpress.', '', $key);
 
         return function_exists('translate') ? translate($key, $replace === '' || $replace === '0' || $replace === [] ? 'default' : $replace) : $key;
-    }
-}
-
-if (! function_exists('is_secured')) {
-    function is_secured(): bool
-    {
-        return str_contains((string) config('app.url'), 'https://');
-    }
-}
-
-if (! function_exists('menu')) {
-    /**
-     * Get a {@link RecursiveIteratorIterator} for a WordPress menu.
-     *
-     * @param  string  $name  name of the menu to get
-     * @param  int  $depth  how far to recurse down the nodes
-     * @param  int  $mode  flags to pass to the {@link RecursiveIteratorIterator}
-     */
-    function menu(string $name, $depth = -1, int $mode = RecursiveIteratorIterator::SELF_FIRST): RecursiveIteratorIterator
-    {
-        $iterator = new RecursiveIteratorIterator(new RecursiveMenuIterator($name), $mode);
-        $iterator->setMaxDepth($depth);
-
-        return $iterator;
     }
 }
